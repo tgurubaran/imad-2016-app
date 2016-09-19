385205f3fa88641e100a39a6256bf624ac9eb465
tgurubaran <tgurubaran@gmail.com>
2016-09-19 17:45:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,35 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+function createTemplate (data) {
+    var title = data.title;
+    var heading = data.heading;
+    var content = data.content;
+    var date = data.date;
+    
+    var htmlTemplate = `
+    <html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name =viewport content="width-device-width, initial-scale-1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <div>
+        <a href = "/"> Home </a>
+    </div>
+    <h4> ${date} </h4>
+    <hr/>
+    <div class = container>
+    <p> Festival in India - ${content} </p>
+    </div>
+</html>
+    `;
+    return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -18,7 +47,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article1',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
+  res.send(createTemplate(article1));
 });
 
 app.get('/article2',function(req,res){
