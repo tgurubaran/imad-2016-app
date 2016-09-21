130fa368c9703b6d97e1608b04c5ff550720d532
tgurubaran <tgurubaran@gmail.com>
2016-09-21 17:09:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+/*
 var articles = {
     article1 = {
         title: "Article1 - Gurubaran",
@@ -55,6 +56,14 @@ function createTemplate (data) {
     return htmlTemplate;
 }
 
+app.get('/:articleName',function(req,res){
+  var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
+});
+
+*/
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -70,13 +79,6 @@ app.get('/ui/madi.png', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-
-app.get('/:articleName',function(req,res){
-  var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
-});
-
-
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
