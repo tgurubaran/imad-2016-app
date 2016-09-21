064e1cb1069299575118ebb170f536ac45221979
tgurubaran <tgurubaran@gmail.com>
2016-09-21 17:18:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,12 @@ var element = document.getElementById("main-text");
 element.innerHTML = "This is the trick";
 
 var img = document.getElementById("madi");
+var marginLeft = 0;
+var function moveRight() {
+    marginLeft += 10px;
+    img.style.marginLeft = marginLeft + "px";
+}
+
+img.onClick = function () {
+    var interval =setInterval(moveRight,10);
+}
