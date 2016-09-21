1c77679a8f42bf5428dceb15d044b76230208b9b
tgurubaran <tgurubaran@gmail.com>
2016-09-21 17:04:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,7 @@
 console.log('Loaded!');
+
+var element = document.getElementById("main-text");
+element.innerHTML = "This is the trick";
+
+var img = document.getElementById("madi");
+madi.onMouseClick =
